2ae068430df65121fca90c826b3ce18e5cff4d39
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2015-07-15 12:35:10 +0300

    add registration

--- todo_dj/urls.py
+++ todo_dj/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
 
-from items.resources import ItemResource, UserResource, LoginResource
+from items.resources import ItemResource, UserResource, LoginResource, RegistrationResource
 
 api = Api(api_name='v1')
 api.register(ItemResource())
 api.register(UserResource())
 api.register(LoginResource())
+api.register(RegistrationResource())
 
 urlpatterns = patterns('',
     url(r'^api/', include(api.urls)),
