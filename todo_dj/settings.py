fd6a3d199e6ed419d194492150dec2b21bcdeb56
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2015-07-30 10:27:45 +0300

    change db settings

--- todo_dj/settings.py
+++ todo_dj/settings.py
@@ -60,10 +60,16 @@ WSGI_APPLICATION = 'todo_dj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'todo_db',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
